007d3bc3fe7b926a68ad0d4076f7fea71684ee73
saady <saadydev@gmail.com>
2025-11-14 19:18:23 -0500

    turborepo finally setup and tailwind nv4 now working too

--- dreamplay/apps/web/next.config.js
+++ dreamplay/apps/web/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["react", "react-dom"],
+  },
+};
 
 export default nextConfig;
